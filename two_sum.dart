50e675b735139d39aceb4b37839a21a774fc27ec
Tetsuo.Oda <tetsuo21ad@gmail.com>
2024-02-26 17:40:59 +0900

    他の回答を受けて修正

--- two_sum.dart
+++ two_sum.dart
@@ -1,20 +1,16 @@
 class Solution {
   List<int> twoSum(List<int> numberList, int target) {
     for (var i = 0; i < numberList.length; i++) {
-      /// i より後の要素を格納する配列。
-      final sublist = numberList.sublist(i + 1);
-      for (var j = 0; j < sublist.length; j++) {
-        if (numberList[i] + sublist[j] == target) {
-          /// return する配列の2つ目の要素の index.
-          /// 
-          /// numberList における index を表す。
-          final secondIndex = i + j + 1;
-          return [i, secondIndex];
+
+      // j の初期値を i + 1 とすることで、 i より後の値のみチェックできるようにしている。
+      for (var j = i + 1; j < numberList.length; j++) {
+        if (numberList[i] + numberList[j] == target) {
+          return [i, j];
         }
       }
     }
 
     // 問題の前提として答えが存在するので、適当な（意味のない）値を返している。
-    return [0, 0];
+    return [];
   }
 }
